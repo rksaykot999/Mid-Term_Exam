87e679dde006141f3ec1c337600744de42995481
rksaykot999 <rksaikatkhan999@gmail.com>
2024-10-18 20:00:59 +0600

    6th commit

--- Question_No-2/simple-website/src/App.jsx
+++ Question_No-2/simple-website/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Footer from './components/Footer'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const cards = [0, 1, 2, 3, 4, 5, 6, 7,8 ,9 , 10, 11, 12, 13, 14, 15]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <Navbar></Navbar>
+      <div className='text-center text-6xl font-bold m-10'>Card Section</div>
+      <div className='grid grid-cols-4 gap-8'>
+        {
+          cards.map((item) => <Cart />)
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='h-screen'>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Footer></Footer>
     </>
   )
 }
